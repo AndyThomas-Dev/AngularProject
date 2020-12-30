62ae7a87cfe5c0911d6b53ecca6afea362ae01ca
AndyThomas-Dev <andythomas.dev@gmail.com>
2020-12-30 13:03:53 +0000

    improved css and changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Andy\'s Tour of Heroes';
+  title = 'Andy\'s Angular Project';
 }
