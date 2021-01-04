488507581437d2a2f0bdc46ca2b33e593ddfa937
AndyThomas-Dev <andythomas.dev@gmail.com>
2021-01-04 13:57:37 +0000

    started top trumps UI

--- src/app/trumps/trumps.component.ts
+++ src/app/trumps/trumps.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-trumps',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrumpsComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  ngOnInit(): void {
+  // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  // Create a method to retrieve the Observable<Hero[]> from the service.
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
